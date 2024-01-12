34e2b0f3af5873a973797555cb99991a1664f70d
Juan Azocar <juazocar@profesor.duoc.cl>
2024-01-12 20:38:14 -0300

    cambios en archivo js

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,1 +1,14 @@
-function login(){ event.preventDefault();var usuario  = document.getElementById("exampleInputEmail1").value;var password = document.getElementById("exampleInputPassword1").value;console.log("Mensaje desde la consola: ");console.log("Usuario: "+usuario+", Password: "+password);if(usuario == ''){alert("El email no puede ir vacío");} else {document.getElementById("myForm").submit();}    }
+function login(){ 
+    e.preventDefault();var usuario  = document.getElementById("exampleInputEmail1").value;var password = document.getElementById("exampleInputPassword1").value;
+    
+// alert("Usuario: "+usuario+", Password: "+password);
+
+    console.log("Mensaje desde la consola: ");console.log("Usuario: "+usuario+", Password: "+password);
+
+    if(usuario == ''){
+        alert("El email no puede ir vacío");
+    } else {
+        document.getElementById("myForm").submit();
+    }
+      
+}
